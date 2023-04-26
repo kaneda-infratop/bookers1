cedb6aa625ebc081737918168f45f673638b273c
EC2 Default User <ec2-user@ip-172-31-41-101.ap-northeast-1.compute.internal>
2023-04-26 08:39:16 +0000

    no error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,15 +27,28 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id])
   end
 
+  #def update
+  #  @book=Book.find(params[:id])
+  #  book = @book.update(book_params)
+  #  if book.save
+  #    flash[:notice] = "successfully"
+  #    redirect_to "/books/#{book.id}"
+  #  else
+  #    render :edit
+  #  end
+  #end
+
   def update
-    @book=Book.new(book_params)
-    if @book.save
-      book=Book.find(params[:id])
-      book.update(book_params)
-      flash[:notice] = "successfully"
-      redirect_to "/books/#{book.id}"
+    @book = Book.find(params[:id])
+    if @book.title.blank? || @book.body.blank?
+      flash.now[:alert] = "Title and body can't be blank"
+      render :edit
     else
-      render :new
+      if @book.update(book_params)
+        redirect_to @book, notice: 'Book was successfully updated.'
+      else
+        render :edit
+      end
     end
   end
 
